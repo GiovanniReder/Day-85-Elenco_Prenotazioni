17d0af683198c839dbd6d0740026919912cef784
Giovanni <gio.reder@gmail.com>
2024-06-21 15:08:34 +0200

    Update UtenteService.java

--- src/main/java/giovanni/GestionePrenotazioni/service/UtenteService.java
+++ src/main/java/giovanni/GestionePrenotazioni/service/UtenteService.java
@@ -1,5 +1,6 @@
 package giovanni.GestionePrenotazioni.service;
 
+import giovanni.GestionePrenotazioni.entitites.Utente;
 import giovanni.GestionePrenotazioni.repository.UtenteRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,11 @@ import org.springframework.stereotype.Service;
 public class UtenteService {
 
     private UtenteRepository utenteRepository;
+
+    public void saveUtente(Utente utente) {
+
+        utenteRepository.save(utente);
+        System.out.println();
+    }
+     
 }
